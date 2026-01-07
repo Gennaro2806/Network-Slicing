4509e122f9c92c699401c32edd4727f028bb94be
Gennaro_Iannicelli <92026374+Gennaro2806@users.noreply.github.com>
2026-01-07 22:18:04 +0100

    Ritorno alla vecchia versione funzionante

--- controllers/service_slicing.py
+++ controllers/service_slicing.py
@@ -1,15 +1,9 @@
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
-from ryu.controller.handler import set_ev_cls
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 
-from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet
-from ryu.lib.packet import ether_types
-from ryu.lib.packet import udp
-from ryu.lib.packet import tcp
-from ryu.lib.packet import icmp
+from ryu.lib.packet import packet, ethernet, ether_types, udp, tcp, icmp
 
 
 class ServiceSlicing(app_manager.RyuApp):
@@ -30,14 +24,8 @@ class ServiceSlicing(app_manager.RyuApp):
 
         # Tabelle MAC → porta per gli switch di bordo
         self.mac_to_port = {
-            1: {
-                "00:00:00:00:00:01": 3,
-                "00:00:00:00:00:02": 4
-            },
-            4: {
-                "00:00:00:00:00:03": 3,
-                "00:00:00:00:00:04": 4
-            },
+            1: {"00:00:00:00:00:01": 3, "00:00:00:00:00:02": 4},
+            4: {"00:00:00:00:00:03": 3, "00:00:00:00:00:04": 4},
         }
 
         # Porta UDP utilizzata per identificare una slice dedicata
@@ -46,11 +34,11 @@ class ServiceSlicing(app_manager.RyuApp):
         # Porte di uscita associate a ciascuna slice per switch
         self.slice_ports = {
             1: {1: 1, 2: 2},
-            4: {1: 1, 2: 2}
+            4: {1: 1, 2: 2},
         }
 
         # Switch di accesso alla rete
-        self.end_swtiches = [1, 4]
+        self.end_switches = [1, 4]
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -59,57 +47,45 @@ class ServiceSlicing(app_manager.RyuApp):
         la regola di table-miss.
         """
         datapath = ev.msg.datapath
-        ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
+        ofproto = datapath.ofproto
 
         match = parser.OFPMatch()
         actions = [
-            parser.OFPActionOutput(
-                ofproto.OFPP_CONTROLLER,
-                ofproto.OFPCML_NO_BUFFER
-            )
+            parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)
         ]
         self.add_flow(datapath, 0, match, actions)
+        self.logger.info("Switch %s connected - table-miss installed", datapath.id)
 
     def add_flow(self, datapath, priority, match, actions):
         """
         Installa una regola di forwarding sullo switch.
         """
-        ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
+        ofproto = datapath.ofproto
 
-        inst = [
-            parser.OFPInstructionActions(
-                ofproto.OFPIT_APPLY_ACTIONS,
-                actions
-            )
-        ]
-
-        mod = parser.OFPFlowMod(
-            datapath=datapath,
-            priority=priority,
-            match=match,
-            instructions=inst
-        )
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        mod = parser.OFPFlowMod(datapath=datapath,
+                                priority=priority,
+                                match=match,
+                                instructions=inst)
         datapath.send_msg(mod)
 
-    def _send_package(self, msg, datapath, in_port, actions):
+    def _send_packet(self, msg, datapath, in_port, actions):
         """
         Inoltra il pacchetto verso la porta selezionata.
         """
-        data = None
+        parser = datapath.ofproto_parser
         ofproto = datapath.ofproto
-
+        data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
 
-        out = datapath.ofproto_parser.OFPPacketOut(
-            datapath=datapath,
-            buffer_id=msg.buffer_id,
-            in_port=in_port,
-            actions=actions,
-            data=data,
-        )
+        out = parser.OFPPacketOut(datapath=datapath,
+                                  buffer_id=msg.buffer_id,
+                                  in_port=in_port,
+                                  actions=actions,
+                                  data=data)
         datapath.send_msg(out)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
@@ -120,7 +96,9 @@ class ServiceSlicing(app_manager.RyuApp):
         """
         msg = ev.msg
         datapath = msg.datapath
+        dpid = datapath.id
         ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
         in_port = msg.match["in_port"]
 
         pkt = packet.Packet(msg.data)
@@ -132,91 +110,76 @@ class ServiceSlicing(app_manager.RyuApp):
 
         dst = eth.dst
         src = eth.src
-        dpid = datapath.id
 
-        # Forwarding basato su MAC per gli switch di bordo
-        if dpid in self.mac_to_port:
-            if dst in self.mac_to_port[dpid]:
-                out_port = self.mac_to_port[dpid][dst]
-                actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
-                match = datapath.ofproto_parser.OFPMatch(eth_dst=dst)
-                self.add_flow(datapath, 1, match, actions)
-                self._send_package(msg, datapath, in_port, actions)
-
-        # Slice 1: traffico UDP su porta dedicata
-        elif (pkt.get_protocol(udp.udp)
-              and pkt.get_protocol(udp.udp).dst_port == self.slice_TCport):
-            slice_number = 1
-            out_port = self.slice_ports[dpid][slice_number]
-
-            match = datapath.ofproto_parser.OFPMatch(
-                in_port=in_port,
-                eth_dst=dst,
-                eth_type=ether_types.ETH_TYPE_IP,
-                ip_proto=0x11,  # UDP
-                udp_dst=self.slice_TCport,
-            )
-
-            actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
+        # -------- Edge MAC forwarding --------
+        if dpid in self.mac_to_port and dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+            actions = [parser.OFPActionOutput(out_port)]
+            match = parser.OFPMatch(eth_dst=dst)
             self.add_flow(datapath, 2, match, actions)
-            self._send_package(msg, datapath, in_port, actions)
+            self._send_packet(msg, datapath, in_port, actions)
+            return
 
-        # Slice 2: traffico UDP generico
-        elif pkt.get_protocol(udp.udp):
-            slice_number = 2
-            out_port = self.slice_ports[dpid][slice_number]
+        # -------- UDP traffic --------
+        udp_pkt = pkt.get_protocol(udp.udp)
+        if udp_pkt:
+            slice_number = 1 if udp_pkt.dst_port == self.slice_TCport else 2
+            out_port = self.slice_ports.get(dpid, {}).get(slice_number, ofproto.OFPP_FLOOD)
 
-            match = datapath.ofproto_parser.OFPMatch(
+            match = parser.OFPMatch(
                 in_port=in_port,
                 eth_dst=dst,
                 eth_src=src,
                 eth_type=ether_types.ETH_TYPE_IP,
                 ip_proto=0x11,  # UDP
-                udp_dst=pkt.get_protocol(udp.udp).dst_port,
+                udp_dst=udp_pkt.dst_port
             )
+            actions = [parser.OFPActionOutput(out_port)]
+            priority = 2 if slice_number == 1 else 1
+            self.add_flow(datapath, priority, match, actions)
+            self._send_packet(msg, datapath, in_port, actions)
+            return
 
-            actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
-            self.add_flow(datapath, 1, match, actions)
-            self._send_package(msg, datapath, in_port, actions)
-
-        # Slice 2: traffico TCP
-        elif pkt.get_protocol(tcp.tcp):
+        # -------- TCP traffic --------
+        tcp_pkt = pkt.get_protocol(tcp.tcp)
+        if tcp_pkt:
             slice_number = 2
-            out_port = self.slice_ports[dpid][slice_number]
+            out_port = self.slice_ports.get(dpid, {}).get(slice_number, ofproto.OFPP_FLOOD)
 
-            match = datapath.ofproto_parser.OFPMatch(
+            match = parser.OFPMatch(
                 in_port=in_port,
                 eth_dst=dst,
                 eth_src=src,
                 eth_type=ether_types.ETH_TYPE_IP,
-                ip_proto=0x06,  # TCP
+                ip_proto=0x06  # TCP
             )
-
-            actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
+            actions = [parser.OFPActionOutput(out_port)]
             self.add_flow(datapath, 1, match, actions)
-            self._send_package(msg, datapath, in_port, actions)
+            self._send_packet(msg, datapath, in_port, actions)
+            return
 
-        # Slice 2: traffico ICMP
-        elif pkt.get_protocol(icmp.icmp):
+        # -------- ICMP traffic --------
+        icmp_pkt = pkt.get_protocol(icmp.icmp)
+        if icmp_pkt:
             slice_number = 2
-            out_port = self.slice_ports[dpid][slice_number]
+            out_port = self.slice_ports.get(dpid, {}).get(slice_number, ofproto.OFPP_FLOOD)
 
-            match = datapath.ofproto_parser.OFPMatch(
+            match = parser.OFPMatch(
                 in_port=in_port,
                 eth_dst=dst,
                 eth_src=src,
                 eth_type=ether_types.ETH_TYPE_IP,
-                ip_proto=0x01,  # ICMP
+                ip_proto=0x01  # ICMP
             )
-
-            actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
+            actions = [parser.OFPActionOutput(out_port)]
             self.add_flow(datapath, 1, match, actions)
-            self._send_package(msg, datapath, in_port, actions)
+            self._send_packet(msg, datapath, in_port, actions)
+            return
 
-        # Forwarding di default per switch intermedi
-        elif dpid not in self.end_swtiches:
+        # -------- Core switch forwarding --------
+        if dpid not in self.end_switches:
             out_port = ofproto.OFPP_FLOOD
-            actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
-            match = datapath.ofproto_parser.OFPMatch(in_port=in_port)
+            actions = [parser.OFPActionOutput(out_port)]
+            match = parser.OFPMatch(in_port=in_port)
             self.add_flow(datapath, 1, match, actions)
-            self._send_package(msg, datapath, in_port, actions)
+            self._send_packet(msg, datapath, in_port, actions)
--- controllers/topology_slicing.py
+++ controllers/topology_slicing.py
@@ -1,7 +1,6 @@
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
-from ryu.controller.handler import set_ev_cls
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 
 from ryu.lib.packet import packet
@@ -11,51 +10,44 @@ from ryu.lib.packet import ether_types
 
 class TopologySlicingMacToPort(app_manager.RyuApp):
     """
-    Controller SDN basato su Ryu che implementa il Topology Slicing.
-    La comunicazione è consentita solo tra coppie di host predefinite,
-    forzando l'utilizzo di percorsi specifici all'interno della topologia.
+    Controller SDN che implementa il Topology Slicing
+    basato su MAC address e percorsi consentiti.
     """
 
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        """
-        Inizializza il controller e le strutture dati necessarie
-        per il controllo delle slice topologiche.
-        """
         super(TopologySlicingMacToPort, self).__init__(*args, **kwargs)
 
-        # Tabella di apprendimento MAC → porta per ciascuno switch
+        # Tabella MAC → porta per ogni switch
         self.mac_to_port = {}
 
-        # Indirizzi MAC degli host
+        # MAC address degli host
         self.mac_h1 = "00:00:00:00:00:01"
         self.mac_h2 = "00:00:00:00:00:02"
         self.mac_h3 = "00:00:00:00:00:03"
         self.mac_h4 = "00:00:00:00:00:04"
 
-        # Coppie di host autorizzate alla comunicazione
+        # Coppie di host autorizzate (slice)
         self.allowed_pairs = {
             (self.mac_h1, self.mac_h3),
             (self.mac_h3, self.mac_h1),
             (self.mac_h2, self.mac_h4),
-            (self.mac_h4, self.mac_h2)
+            (self.mac_h4, self.mac_h2),
         }
 
-        # Switch ammessi per ciascuna slice topologica
-        self.upper_path_switches = {1, 2, 4}  # Slice video
-        self.lower_path_switches = {1, 3, 4}  # Slice HTTP
+        # Switch ammessi per ciascuna slice
+        self.upper_path_switches = {1, 2, 4}  # H1 ↔ H3
+        self.lower_path_switches = {1, 3, 4}  # H2 ↔ H4
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
         """
-        Gestisce l'evento di connessione di uno switch al controller.
-        Installa una regola di table-miss per inoltrare al controller
-        i pacchetti non corrispondenti ad alcun flow.
+        Installa la regola di table-miss.
         """
         datapath = ev.msg.datapath
-        ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
+        ofproto = datapath.ofproto
 
         match = parser.OFPMatch()
         actions = [
@@ -64,7 +56,13 @@ class TopologySlicingMacToPort(app_manager.RyuApp):
                 ofproto.OFPCML_NO_BUFFER
             )
         ]
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+
+        inst = [
+            parser.OFPInstructionActions(
+                ofproto.OFPIT_APPLY_ACTIONS,
+                actions
+            )
+        ]
 
         mod = parser.OFPFlowMod(
             datapath=datapath,
@@ -76,14 +74,19 @@ class TopologySlicingMacToPort(app_manager.RyuApp):
 
     def add_flow(self, datapath, priority, match, actions, buffer_id=None):
         """
-        Installa una regola di forwarding sullo switch.
+        Installa una regola di forwarding.
         """
-        ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
+        ofproto = datapath.ofproto
 
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        inst = [
+            parser.OFPInstructionActions(
+                ofproto.OFPIT_APPLY_ACTIONS,
+                actions
+            )
+        ]
 
-        if buffer_id:
+        if buffer_id is not None:
             mod = parser.OFPFlowMod(
                 datapath=datapath,
                 buffer_id=buffer_id,
@@ -104,72 +107,74 @@ class TopologySlicingMacToPort(app_manager.RyuApp):
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         """
-        Gestisce i pacchetti ricevuti dal controller e applica
-        le politiche di slicing topologico.
+        Gestisce i pacchetti in ingresso applicando
+        il topology slicing.
         """
         msg = ev.msg
         datapath = msg.datapath
         dpid = datapath.id
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-        in_port = msg.match['in_port']
+        in_port = msg.match["in_port"]
 
         pkt = packet.Packet(msg.data)
-        eth = pkt.get_protocols(ethernet.ethernet)[0]
-        dst = eth.dst
-        src = eth.src
+        eth = pkt.get_protocol(ethernet.ethernet)
 
-        # Ignora pacchetti LLDP
+        # Ignora LLDP
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
             return
 
-        # Inizializza la tabella MAC per lo switch corrente
+        src = eth.src
+        dst = eth.dst
+
+        # Inizializza la tabella MAC per questo switch
         self.mac_to_port.setdefault(dpid, {})
 
-        # Apprendimento MAC → porta
+        # Apprendimento MAC
         self.mac_to_port[dpid][src] = in_port
 
-        # Verifica se la comunicazione è autorizzata
+        # Verifica se la comunicazione è consentita
         if (src, dst) not in self.allowed_pairs:
-            self.logger.info(
-                "Blocking packet from %s to %s on switch %s",
-                src, dst, dpid
-            )
+            self.logger.info("Flusso bloccato: %s -> %s", src, dst)
             return
 
-        # Controllo del percorso consentito in base alla slice
-        if (src == self.mac_h1 and dst == self.mac_h3) or (src == self.mac_h3 and dst == self.mac_h1):
+        # Controllo slice topologica
+        if (src, dst) in {(self.mac_h1, self.mac_h3), (self.mac_h3, self.mac_h1)}:
             if dpid not in self.upper_path_switches:
                 self.logger.info(
-                    "Blocking video slice packet from %s to %s on switch %s",
-                    src, dst, dpid
-                )
-                return
-        elif (src == self.mac_h2 and dst == self.mac_h4) or (src == self.mac_h4 and dst == self.mac_h2):
-            if dpid not in self.lower_path_switches:
-                self.logger.info(
-                    "Blocking http slice packet from %s to %s on switch %s",
-                    src, dst, dpid
+                    "H1-H3 attraversa switch NON ammesso (dpid=%s)", dpid
                 )
                 return
 
-        # Determinazione della porta di uscita
+        if (src, dst) in {(self.mac_h2, self.mac_h4), (self.mac_h4, self.mac_h2)}:
+            if dpid not in self.lower_path_switches:
+                self.logger.info(
+                    "H2-H4 attraversa switch NON ammesso (dpid=%s)", dpid
+                )
+                return
+
+        # Determina la porta di uscita
         if dst in self.mac_to_port[dpid]:
             out_port = self.mac_to_port[dpid][dst]
         else:
             out_port = ofproto.OFPP_FLOOD
 
         actions = [parser.OFPActionOutput(out_port)]
-        match = parser.OFPMatch(in_port=in_port, eth_src=src, eth_dst=dst)
 
-        # Installazione del flow
+        # Installa la regola
+        match = parser.OFPMatch(
+            in_port=in_port,
+            eth_src=src,
+            eth_dst=dst
+        )
+
         if msg.buffer_id != ofproto.OFP_NO_BUFFER:
             self.add_flow(datapath, 1, match, actions, msg.buffer_id)
             return
         else:
             self.add_flow(datapath, 1, match, actions)
 
-        # Inoltro del pacchetto
+        # Invia il pacchetto
         data = msg.data if msg.buffer_id == ofproto.OFP_NO_BUFFER else None
         out = parser.OFPPacketOut(
             datapath=datapath,
@@ -178,5 +183,4 @@ class TopologySlicingMacToPort(app_manager.RyuApp):
             actions=actions,
             data=data
         )
-
         datapath.send_msg(out)
